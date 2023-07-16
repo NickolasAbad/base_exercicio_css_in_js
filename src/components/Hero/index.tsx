f3459dd73a57384307e4da3902177e72c450b9ca
Nickolas Abad <123907645+NickolasAbad@users.noreply.github.com>
2023-07-16 19:51:40 -0300

    finishing all modifications (css -> styled-components)

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from '../../styles'
+import { Case, Formulario, HeroTitle } from './styles'
+
+const Hero = () => (
+  <Formulario>
+    <Container>
+      <Case>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </Case>
+    </Container>
+  </Formulario>
+)
+
+export default Hero
